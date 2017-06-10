1d945d0580000a72e117326b700fa63576315fa5
Johnny Chen <johnny.chen54@gmail.com>
2017-06-10 10:20:27 -0700

    Test React Modal

--- src/client/components/product_list/ProductGridList.js
+++ src/client/components/product_list/ProductGridList.js
@@ -4,6 +4,7 @@ import IconButton from 'material-ui/IconButton';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Modal from 'react-modal';
 
 /**
  * This example demonstrates "featured" tiles, using the `rows` and `cols` props to adjust the size of the tile.
@@ -35,31 +36,33 @@ class ProductGridList extends Component {
     };
       if (this.props.results.length > 0) {
         return (
-          <div style={styles.root}>
-            <GridList
-              cols={2}
-              cellHeight={200}
-              padding={1}
-              style={styles.gridList}
-              >
-                {this.props.results.map((tile) => (
-                  <GridTile
-                    onClick={this.props.handleProductClicked}
-                    key={tile.LargeImage.URL}
-                    title={tile.ItemAttributes.Title}
-                    subtitle={tile.ItemAttributes.Author}
-                    actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
-                    actionPosition="left"
-                    titlePosition="top"
-                    titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
-                    // cols={tile.featured ? 2 : 1}
-                    // rows={tile.featured ? 2 : 1}
-                    >
-                      <img src={tile.LargeImage.URL} />
-                      {/* <img src={tile.ImageSets.ImageSet[0].ThumbnailImage.URL} /> */}
-                    </GridTile>
-                  ))}
-                </GridList>
+          <div>
+            <div style={styles.root}>
+              <GridList
+                cols={2}
+                cellHeight={200}
+                padding={1}
+                style={styles.gridList}
+                >
+                  {this.props.results.map((tile) => (
+                    <GridTile
+                      onClick={this.props.handleProductClicked}
+                      key={tile.LargeImage.URL}
+                      title={tile.ItemAttributes.Title}
+                      subtitle={tile.ItemAttributes.Author}
+                      actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                      actionPosition="left"
+                      titlePosition="top"
+                      titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+                      // cols={tile.featured ? 2 : 1}
+                      // rows={tile.featured ? 2 : 1}
+                      >
+                        <img src={tile.LargeImage.URL} />
+                        {/* <img src={tile.ImageSets.ImageSet[0].ThumbnailImage.URL} /> */}
+                      </GridTile>
+                    ))}
+                  </GridList>
+                </div>
               </div>
           )
       } else {
